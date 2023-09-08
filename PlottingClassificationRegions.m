1ad63e3d140c8514d9082d330d9c3b8aa9ec31be
leapor <39188519+leapor@users.noreply.github.com>
2023-09-08 14:46:49 +0200

    Update PlottingClassificationRegions.m

--- PlottingClassificationRegions.m
+++ PlottingClassificationRegions.m
@@ -5,6 +5,7 @@ close all; clear all;
 % Includes adding letters for each row of subplots and class names. Also 
 % plots isobaths for 250 and 1000 m.
 % ISOBATHS TAKE A LONG TIME TO PLOT!
+% Needs m_map and othercolor packages.
 
 % -------------------------------------------------
 % --- File names ---
